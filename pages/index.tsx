e8c116c245c5d5b32df5c2127617091d743caf62
Lana Wong <lana_1123@hotmail.com>
2022-04-22 15:00:12 +0800

    feat: rearranged the button

--- pages/index.tsx
+++ pages/index.tsx
@@ -223,7 +223,7 @@ const Home: NextPage = () => {
 
             }
 
-            else {
+            else if(paths[i][0] == 'C') {
 
 
                 point = [];
@@ -264,7 +264,7 @@ const Home: NextPage = () => {
           className="inline-flex items-center px-4 py-2 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
         >
           Generate sorted blob
-        </button>  <button
+        </button><button
           onClick={() => extractPath()}
           type="button"
           className="inline-flex items-center px-4 py-2 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
